a83b9da659842433bc3efd5a168c56664fb784d5
TAHA <154291824+Taha-Aheras@users.noreply.github.com>
2024-01-26 22:58:03 +0530

    Add files via upload

---
+++ Taha-project-dev-tool/ADMIN/connection.php
@@ -0,0 +1,14 @@
+<?php
+$dbhost ='localhost';
+$dbuser = 'root';
+$dbpass = '';
+$db='pmas';
+
+$conn= mysqli_connect($dbhost,$dbuser,$dbpass);
+if($conn->connect_error){
+    die("Connection failed : " . $conn->connect_error);
+}
+mysqli_select_db($conn, $db); 
+               
+?>
+<!-- Project made by taha-Aheras-->
